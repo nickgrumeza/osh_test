7d9619735abd452a904178fcc9da31838a377157
Benjamin_Baker <boss.grumeza@gmail.com>
2021-05-16 21:35:47 +0300

    Inserting into DB and testing

--- src/main/java/osh/Read.java
+++ src/main/java/osh/Read.java
@@ -6,14 +6,57 @@ import org.supercsv.io.*;
 import org.supercsv.prefs.CsvPreference;
 import java.io.*;
 import java.sql.*;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 
 public class Read{
-    private final String jdbcUrl = "jdbc:sqlite::memory:";
-    private Connection con = null;
+    static final String csvFilePath = "D:/Programming/Osh/Interview-task-data-osh.csv";
+    private static final String jdbcUrl = "jdbc:sqlite::memory:";
+    private static Connection con = null;
 
-    public static void readCSVFile(String csvFilePath){
-        
-        CellProcessor[] processors = new CellProcessor[] {
+    public static int recordsRecieved = 0;
+    public static int recordsSuccussful = 0;
+    public static int recordsFailed = 0;
+    public static String[] beanArr;
+
+    public static void openConnection() throws SQLException{
+        if (con == null || con.isClosed()){
+            con = DriverManager.getConnection(jdbcUrl);
+        }
+    }
+
+    public static void closeConnection() throws SQLException 
+    {
+        con.close();
+    }
+
+    public static void createTable(){
+        try{
+            final Statement statement = con.createStatement();
+
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS X"
+                + "(A TEXT," 
+                + " B TEXT," 
+                + " C TEXT," 
+                + " D TEXT," 
+                + " E TEXT," 
+                + " F TEXT," 
+                + " G TEXT," 
+                + " H TEXT," 
+                + " I TEXT," 
+                + " J TEXT);");                 
+        }   catch (SQLException e){
+            e.getMessage();
+        }
+    }
+
+    public static void readAndInsert() throws SQLException 
+    {          
+        PreparedStatement pstatement = con.prepareStatement("INSERT INTO X(A,B,C,D,E,F,G,H,I,J) "
+                    + "VALUES(?,?,?,?,?,?,?,?,?,?);");
+
+        CellProcessor[] processors = new CellProcessor[]{
             new Optional(),
             new Optional(),
             new Optional(),
@@ -21,30 +64,66 @@ public class Read{
             new Optional(),
             new Optional(),
             new Optional(),
-            new Optional(new ParseBool()),
-            new Optional(new ParseBool()),
+            new Optional(),
+            new Optional(),
             new Optional()
-        };        
+        };    
       
         try(ICsvBeanReader beanReader = new CsvBeanReader(new FileReader(csvFilePath), 
-        CsvPreference.STANDARD_PREFERENCE);)
-        {
+        CsvPreference.STANDARD_PREFERENCE);){
             final String[] headers = beanReader.getHeader(true);
-            Data dataBean = null;
-            while ((dataBean = beanReader.read(Data.class, headers, processors)) != null) {
-                System.out.printf("%s %s %s %s %s %s %s %s %s %s",
-                dataBean.getA(), dataBean.getB(), dataBean.getC(), 
-                dataBean.getD(), dataBean.getE(), dataBean.getF(),
-                dataBean.getG(), dataBean.getH(), dataBean.getI(),
-                dataBean.getJ());
-                System.out.println();
+                        
+            PojoBean beanData = null;
+            while ((beanData = beanReader.read(PojoBean.class, headers, processors)) != null) {
+                if (beanReader.getLineNumber() <= 11){
+                    if ((beanData.getA() != null) && (beanData.getB() != null) && (beanData.getC() != null)
+                            && (beanData.getD() != null) && (beanData.getE() != null) && (beanData.getF() != null) 
+                            && (beanData.getG() != null) && (beanData.getH() != null) && (beanData.getI() != null) 
+                            && (beanData.getJ() != null))
+                    {
+                        recordsSuccussful++;
+                        pstatement.setString(1, beanData.getA());
+                        pstatement.setString(2, beanData.getB());
+                        pstatement.setString(3, beanData.getC());
+                        pstatement.setString(4, beanData.getD());
+                        pstatement.setString(5, beanData.getE());
+                        pstatement.setString(6, beanData.getF());
+                        pstatement.setString(7, beanData.getG());
+                        pstatement.setString(8, beanData.getH());
+                        pstatement.setString(9, beanData.getI());
+                        pstatement.setString(10, beanData.getJ());
+                        pstatement.executeUpdate();
+                    } else {
+                        recordsFailed++;
+                    }
+                } else {
+                    break;
+                }
+                recordsRecieved++;
             }
-            
             beanReader.close();
         } catch (FileNotFoundException ex) {
             System.err.println("Could not find the CSV file: " + ex);
         } catch (IOException ex) {
             System.err.println("Error reading the CSV file: " + ex);
         }       
-    }     
+    } 
+    
+    public static void testDb() {
+        try {
+            final Statement stat = con.createStatement();
+            ResultSet res = stat.executeQuery("SELECT * FROM X;");
+            ResultSetMetaData resMetaData = res.getMetaData();
+            int numColumns = resMetaData.getColumnCount();
+          
+            while (res.next()){
+                for (int i=1; i<=numColumns; i++){
+                    String columnValue = res.getString(i);
+                    System.out.print(columnValue + " ");
+                }
+            }
+        } catch (SQLException e){
+            e.getMessage();
+        }   
+    }
 }
